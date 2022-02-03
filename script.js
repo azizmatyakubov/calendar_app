c6b10dc843f930fcabce91de129aa1ed6541aa22
AZIZ <a.matyaqubov1471@gmail.com>
2022-02-03 16:00:30 +0100

    add functions

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
+// calendar database
+let calendar = {
+    '1': [{'description': 'Call Eric'}]}
+
+
 window.onload = () => {
-    day()
+    createDays()
 }
 
-const day = () => {
+const createDays = () => {
 
-    let calendar = document.getElementById('calendar')
-    console.log(calendar)
-    for(let i=1; i<=28; i++) {
-        let day = document.createElement('div')
-        day.classList.add('day')
-        day.innerText = i
-        day.addEventListener('click', clickDay)
-        calendar.appendChild(day)
+    let calendarDiv = document.getElementById('calendar')
+    let totalDays = 28
+
+    for(let dayOfTheMonth=1; dayOfTheMonth<=totalDays; dayOfTheMonth++) {
+        let dayDiv = document.createElement('div')
+        dayDiv.classList.add('day')
+        dayDiv.innerText = dayOfTheMonth
+        dayDiv.addEventListener('click', clickDay)
+        calendarDiv.appendChild(dayDiv)
     }
 
+    calendar[dayOfTheMonth] = []
 
 }
 
@@ -23,4 +30,22 @@ const clickDay = (e) => {
         document.querySelector('.selected').classList.remove('selected')
     }
     e.target.classList.add('selected')
+
+    let selectedDaysNumber = e.target.innerText
+    displayMeetingsForDay(selectedDaysNumber)
+}
+
+
+const displayMeetingsForDay = (selectedDaysNumber) => {
+    let meetingsListForToday = calendar[selectedDaysNumber]
+
+    let todos = document.getElementsByClassName('todos')
+    todos.innerText = ''
+
+    for(let meeting of meetingsListForToday) {
+        let h3ForList = document.createElement('h3')
+        h3ForList.classList.add('todos-title')
+        h3ForList.innerText = meeting.description
+        todos.appendChild(h3ForList)
+    }
 }
