f8bc535c87873052c4484e97973ba27572d6c409
AZIZ <a.matyaqubov1471@gmail.com>
2022-02-04 15:47:23 +0100

    finish version 1.0

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // calendar database
 let calendar = {
-    '1': [{'description': 'Call Eric', 'time': '19:30'},{'description': 'Call Eric', 'time': '19:30'}, {'description': 'Call Eric', 'time': '19:30'}]}
+
+}
 
 
 window.onload = () => {
@@ -9,25 +10,42 @@ window.onload = () => {
 
 const createDays = () => {
 
+    // Get node of calendar div
     let calendarDiv = document.getElementById('calendar')
+
+    // Total days
     let totalDays = 28
 
     for(let dayOfTheMonth=1; dayOfTheMonth<=totalDays; dayOfTheMonth++) {
+        
+        // set dafault array to every day
+        calendar[dayOfTheMonth] = []
+        
+        // Create new div
         let dayDiv = document.createElement('div')
+        
+        // Add class to new div
         dayDiv.classList.add('day')
+
+        // Add innerText to new div ----> dayOfTheMonth from loop
         dayDiv.innerText = dayOfTheMonth
+
+        // Add event listener to new div
         dayDiv.addEventListener('click', clickDay)
+
+        // Append child to calendar div
         calendarDiv.appendChild(dayDiv)
+        
     }
-
-    //calendar[dayOfTheMonth] = []
-
 }
 
 
 const clickDay = (e) => {
-    if(document.querySelector('.selected') !== null) {
-        document.querySelector('.selected').classList.remove('selected')
+
+    let currentlySelectedDayNode =  document.querySelector('.selected')
+
+    if(currentlySelectedDayNode !== null) {
+        currentlySelectedDayNode.classList.remove('selected')
     }
     e.target.classList.add('selected')
 
@@ -35,17 +53,18 @@ const clickDay = (e) => {
     displayMeetingsForDay(selectedDaysNumber)
 }
 
-
 const displayMeetingsForDay = (selectedDaysNumber) => {
     
-    let meetingsListForToday = calendar[selectedDaysNumber]
+    meetingsListForToday = calendar[selectedDaysNumber]
+    console.log(meetingsListForToday)
 
     // Remove innerText of todos div
     let appointments = document.getElementsByClassName('appointments')[0]
     appointments.innerText = ''
+    console.log(appointments)
 
     // Looping each element of selected days array
-    for(let i=0; i<meetingsListForToday.length; i++) {
+    for(let meeting of meetingsListForToday) {
 
         // Create Nodes
         let h3ForList = document.createElement('h3')
@@ -57,15 +76,47 @@ const displayMeetingsForDay = (selectedDaysNumber) => {
         divForTodos.classList.add('todos')
 
         // Add innerText
-        h3ForList.innerText = meetingsListForToday[i]['description']
-        divForList.innerText =  meetingsListForToday[i]['time']
+        h3ForList.innerText = meeting['title']
+        divForList.innerText = meeting['time']
       
         // Append children to todos div
         divForTodos.appendChild(h3ForList)
         divForTodos.appendChild(divForList)
-        console.log(divForTodos)
-
+  
         // Append to todos div
         appointments.appendChild(divForTodos)
     }
+}
+
+const addMeeting = () => {
+
+
+    // Get node which has selected class
+    let selectedDay = document.querySelector('.selected')
+
+    if(selectedDay === null) {
+        alert("Select day!")
+        return
+    }
+
+    // Get innerText of selectedDay
+    let selectedDayAsString = selectedDay.innerText
+
+    // Get selectedDays array
+    let meetingsForSelectedDay = calendar[selectedDayAsString]
+    
+    // Get values from user
+    let appointmentTitle = document.getElementById('appointment-title').value
+    let appointmentTime = document.getElementById('appointment-time').value
+
+    // Create new object
+    let newTodos = {
+        'title': appointmentTitle,
+        'time': appointmentTime, 
+    }
+
+    // Push object to array
+    meetingsForSelectedDay.push(newTodos)
+
+    displayMeetingsForDay(selectedDayAsString)
 }
